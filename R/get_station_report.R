f172a004e8d0fe871f2e007ffb3c4872b804e22e
Ousmane Ouedraogo <oousmane@proton.me>
2024-06-12 08:53:43 +0000

    Update get_station_report.R

--- R/get_station_report.R
+++ R/get_station_report.R
@@ -1,16 +1,18 @@
-
-
-#' Get OSCAR station report summary by providing it's identifier
+#' Retrieve WMO OSCAR station report summary
 #' @importFrom janitor clean_names
 #' @importFrom tibble as_tibble
-#' @param identifier
+#' 
+#' @param identifier WIGOS station identifier 
 #'
-#' @return a [tibble][tibble::tibble-package]. Summary report of the station with identifier
+#' @return a [tibble][tibble::tibble-package]. Summary report of the station in a tibble \code{data.frame}
 #' @export
 #'
 #' @examples
 #' library(oscarr)
-#' get_station_report(identifier = "0-20000-0-71758")
+#' wigos_baraboule <- "0-854-0-109"
+#' get_station_report(identifier = wigos_baraboule)
+#'
+#'
 get_station_report <- function(identifier="0-20000-0-71758"){
 c <- pyoscar$OSCARClient()
 c$get_station_report(identifier=identifier,summary = TRUE) %>%
