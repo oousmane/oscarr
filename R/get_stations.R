5fe126fca325c52356b00ad69f32acf3866ca0c5
Ousmane Ouedraogo <oousmane@proton.me>
2024-06-12 08:39:18 +0000

    Update get_stations.R

--- R/get_stations.R
+++ R/get_stations.R
@@ -1,14 +1,19 @@
 
 
-#' Title
+#' Retrieve WMO OSCAR station list for a country
 #'
-#' @param country Official ISO 3C of country name
+#' @param country Official ISO 3166-1 alpha-3 code (3 letters)
 #' @importFrom purrr map_dfr
 #' @importFrom purrr pluck
 #' @importFrom reticulate py_to_r
 #' @importFrom dplyr mutate
 #' @return A tibble. OSCAR Station list of the specified country
 #' @export
+#' @example
+#' library(oscarr)
+#' country <- "NGA" # Nigeria ISO 3166-1 alpha-3 code
+#' nga_stns <- get_stations(country = country)
+#' View(nga_stns)
 #'
 get_stations <- function(country="BFA"){
   c=pyoscar$OSCARClient()
